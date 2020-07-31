9b17bc0d146c900a15da3df9f1f295b237f5ced9
giovany samaca <egsamaca56@gmail.com>
2020-07-30 20:55:48 -0500

    esta version esta con un codigo mas legible

--- peso.py
+++ peso.py
@@ -2,78 +2,70 @@ def mensaje():
     print("\n\n\t\t\t\t¡adios! 👊🏽")
     print("\n\n")
 
+def menu():
+    menu = """       
+                                bienvenido a peso en otro planeta
+            aqui podras averiguar cuanto pesas en cualquier planeta del sistema solar
 
-menu = """       
-                               bienvenido a peso en otro planeta
-           aqui podras averiguar cuanto pesas en cualquier planeta del sistema solar
+    1. mercurio
+    2. venus
+    3. tierra
+    4. marte 
+    5. jupiter
+    6. saturno 
+    7. urano
+    8. neptuno
+    9. pluton
+    10. salir
 
-                                 🌖      1. mercurio        🚀         👾        🌛
-     💫           🌏                    2. venus                    ☄️     ☀️
-                             👾          3. tierra       🌝                          🌙
-                    🌒                  4. marte                       ⭐️
-     ⭐️     🛸                  🚀      5. jupiter       🛸      ✨ 
-                           ☀️             6. saturno                             🌑
-             ✨                         7. urano               ☀️             
-     ☄️                   🛰               8. neptuno     💫         🌎      🛰       
-               🌛                🌑     9. pluton                                 ☄️  
-                                                                🌒          🛸          🛰
-escoge un planeta para averiguar tu peso: """
+    🚀 """
+    print(menu)
 
-opcion = int(input(menu))
-gravedad_terrestre = 9.8
-
-    
-def operacion():# esta es la funcion que cambia el peso digitador el usuario a el peso del planeta que el mismo usuario escogio
-    
+def operacion(gravedad,planeta):# esta es la funcion que cambia el peso digitador el usuario a el peso del planeta que el mismo usuario escogio
+    gravedad_terrestre = 9.8
+    print(f'\nescogiste {planeta} 💥')
     peso_int = float(input("\n¿cuanto pesas? "))
     if peso_int > 0 and peso_int < 150:
        peso = (peso_int * gravedad)/gravedad_terrestre
        peso = str(round(peso, 2))
-       print("¡genial!  tu peso en este planeta es de "+peso+" kg es increible no? 🚀")
+       print("\n\n¡G E N I A L!  tu peso en este planeta es de "+peso+" kg es increible no? 🚀")
        print("\n\n\n")
-       mensaje()
     else:
-        print("🤦‍♂️ ¡nooooo! 😩 ese peso no es valido, intenta otra vez")
+        print("🤦‍♂️ ¡N O O ! 😩 ese peso no es valido, intenta otra vez")
         print("\n\n\n")
 # fin de la funcion
-# inicio condicionales para validar la opcion escogida por el usuario
 
-if opcion == 1:
-    print("\nescogiste mercurio 💥")
-    gravedad = 3.7
-    operacion()
-elif opcion == 2:
-    print("\nescogiste venus 💥")
-    gravedad = 8.87
-    operacion()
-elif opcion == 3:
-    print("\nescogiste tierra 💥")
-    gravedad = 9.8
-    operacion()
-elif opcion == 4:
-    print("\nescogiste marte 💥")
-    gravedad = 3.7
-    operacion()
-elif opcion == 5:
-    print("\nescogiste jupiter 💥")
-    gravedad = 24.7
-    operacion()
-elif opcion == 6:
-    print("\nescogiste saturno 💥")
-    gravedad = 10.4
-    operacion()
-elif opcion == 7:
-    print("\nescogiste urano 💥")
-    gravedad = 8.8
-    operacion()
-elif opcion == 8:
-    print("\nescogiste neptuno 💥")
-    gravedad = 11.1
-    operacion()
-elif opcion == 9:
-    print("\nescogiste pluton 💥")
-    gravedad = 0.62
-    operacion()
-else: 
-    print("🤦‍♂️ ¡nooooo! 😩 esa opcion no es valida intenta de nuevo eligiendo una opcion correcta ")
-    print("\n\n\n")
+def main():
+    opcion =0 
+    while opcion != 10:
+        menu()    
+        opcion = int(input('selecciona una opcion: '))       # inicio condicionales para validar la opcion escogida por el usuario
+        if opcion == 1:
+            operacion(3.7,'mercurio')
+        elif opcion == 2:
+            operacion(8.87,'venus')
+        elif opcion == 3:
+            operacion(9.8,'tierra')
+        elif opcion == 4:
+            operacion(3.7,'marte')
+        elif opcion == 5:
+            operacion(24.7,'jupiter')
+        elif opcion == 6:
+            operacion(10.4,'saturno')
+        elif opcion == 7:
+            operacion(8.8,'urano')
+        elif opcion == 8:
+            operacion(11.1,'neptuno')
+        elif opcion == 9:
+            operacion(0.62,'pluton')
+        elif opcion == 10:
+            mensaje()
+            break
+        else: 
+            print("🤦‍♂️ ¡N O O O! 😩 esa opcion no es valida intenta de nuevo eligiendo una opcion correcta ")
+            print("\n\n\n")
+
+   
+if __name__ == "__main__":
+    main()
+    
